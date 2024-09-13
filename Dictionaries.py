36bf8694681016dc387490a41f380088a4e9f119
Franck300302 <kiemdefranck5@gmail.com>
2024-09-13 17:21:24 +0200

    Day5

---
+++ Dictionaries.py
@@ -0,0 +1,24 @@
+#Task 3.1
+student = {
+    "name" : "KIEMDE",
+    "academic_year" : "pre-msc",
+    "units": [
+        {
+            "name" : "Web Developpment",
+            "credits" : 2,
+            "grade" : "C"
+        },
+        {
+            "name" : "Network and System Administration",
+            "credits" : 2,
+            "grade": "C"
+        },
+        {
+            "name" : "Java",
+            "credits" : 2,
+            "grade" : "C"
+        }
+    ]
+}
+
+print(student)
