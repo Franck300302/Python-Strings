5ed9beafbd666980eb04bac06f3e80aae906cbb6
Franck300302 <kiemdefranck5@gmail.com>
2024-09-20 10:45:21 +0200

    Bases

---
+++ Exo 2.py
@@ -0,0 +1,63 @@
+# n = int(input("Entrez un nombre : "))
+# i = 0
+# while i < n:
+#     print((i+1) * "x" ) 
+#     i = i + 1 
+#     (".\n")
+
+
+# ch = "" 
+# for i in range (4):
+#     ch = ch + str(i)
+# print(ch) 
+
+
+# def affiche_ligne(m):
+#     ch =" "
+#     for i in range(m, 0, - 1):
+#         ch = ch + str(i) +  " "
+#         print(ch)
+#         for j in range (m - 1, 0,):
+#             print(j)
+
+# affiche_ligne(5)
+
+
+# def affiche_ligne(m):
+    
+#     i = 5 
+    
+#     while i < m:
+#         print (i)
+#         i = i - 1 
+
+# affiche_ligne(6)
+
+# a = somme(12, 8)
+
+# def somme(a, b):
+#     resultat = a + b
+#     return resultat
+
+# a = somme(12, 8)
+# print(a)
+
+
+# def somme(a, b, c):
+#     resultat = a + b + c
+#     return  resultat
+
+# a = somme(154, 16, 30)
+# print(a)
+
+T = ["a", "b", "c", "d"]
+
+for i in range (len(T)):
+    print(T[i])
+
+i = 0
+while i < len(T [::]):
+    print(T[i])
+    i = i + 1
+
+
